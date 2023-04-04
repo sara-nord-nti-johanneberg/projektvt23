05a3ae70fbe26bc8b334d1a6f1f4165f76db3c7a
sawa <sara.nord@elev.ga.ntig.se>
2023-04-04 10:51:51 +0200

    inlämning

--- js/script.js
+++ js/script.js
@@ -1,1 +1,18 @@
-// Denna fil skall innehålla JavaScript (JS)
+cardsData.forEach((card) => {
+    const cardElement = document.createElement('div');
+    cardElement.classList.add('card');
+  
+    const cardFrontElement = document.createElement('div');
+    cardFrontElement.classList.add('card-front');
+    cardFrontElement.textContent = card.front;
+  
+    const cardBackElement = document.createElement('div');
+    cardBackElement.classList.add('card-back');
+    cardBackElement.textContent = card.back;
+  
+    cardElement.appendChild(cardFrontElement);
+    cardElement.appendChild(cardBackElement);
+  
+    cardsContainer.appendChild(cardElement);
+  });
+  // Denna fil skall innehålla JavaScript (JS)
